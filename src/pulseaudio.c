bfc6d6c2a14635210e35a6367c3f6eac04231534
c vw <dl1ycf@darc.de>
2025-12-11 18:09:29 +0100

    emergency fix for pulseaudio.c

--- src/pulseaudio.c
+++ src/pulseaudio.c
@@ -419,7 +419,7 @@ int cw_audio_write(RECEIVER *rx, float sample) {
         rx->cwcount--;
       }
 
-      if (latency > AUDIO_LAT_HIGH && rx->cwcount == 0) {
+      if (rx->cwcount == 0 || latency < AUDIO_LAT_LOW) {
         int rc = pa_simple_write(rx->playstream,
                                  rx->local_audio_buffer,
                                  out_buffer_size * sizeof(float) * 2,
