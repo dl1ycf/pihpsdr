e101ea153ac20b31366afcf194455d312bf37d01
c vw <dl1ycf@darc.de>
2026-02-10 14:42:37 +0100

    Small correction to PS menu "Restart" button, when *not* effected while doing a two-tone experiment.

--- src/ps_menu.c
+++ src/ps_menu.c
@@ -535,7 +535,7 @@ static void resume_cb(GtkWidget *widget, gpointer data) {
   // If not auto-adjusting, do not change attenuation value.
   //
   if (transmitter->puresignal) {
-    if (transmitter->auto_on) {
+    if (transmitter->twotone && transmitter->auto_on) {
       transmitter->attenuation = 0;
 
       if (radio_is_remote) {
