b98bf02d8326d4baabc19268a323a15ef3b88550
c vw <dl1ycf@darc.de>
2026-05-22 17:30:38 +0200

    new WDSP files from Warren

--- wdsp/gaussian.h
+++ wdsp/gaussian.h
@@ -1,8 +1,8 @@
-/*	gaussian.h
+/*  gaussian.h
 
 This file is part of a program that implements a Software-Defined Radio.
 
-Copyright (C) 2025 Warren Pratt, NR0V
+Copyright (C) 2025-2026 Warren Pratt, NR0V
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -38,8 +38,8 @@ typedef struct _gaussian
 	int run;					// 0 - filter is OFF; 1 - filter is ON
 	int position;				// position in sequence in which to execute the filter
 	int size;					// input/output buffer size
-	int nc;						// number of filter coefficients; if'0', will be automatically calculated
-	int nc_var;					// '0' filter size will not be adjusted; '1' filter size adjusted per nsigma
+	int nc;						// number of filter coefficients; if set to '0', will be automatically calculated
+	int nc_default;				// value to which 'nc' was last set at instantiation or by a call to set 'nc'
 	double* in;					// pointer to input buffer
 	double* out;				// pointer to output buffer
 	double f_center;			// filter center frequency (Hz)
@@ -48,7 +48,7 @@ typedef struct _gaussian
 	double gain;				// gain to be applied to filter output
 	double scale;				// internal filter scale factor based upon gain
 	double nsigma;				// number of 'sigma' on each side of center to extend the Gaussian curve
-	int mode;					// Mode to get output:	0 => CWL; 1 => CWU; 2 => CWL + CWU
+	int mode;					// Mode to get output:  0 => CWL; 1 => CWU; 2 => CWL + CWU
 	FIRCORE p;					// pointer to partititioned overlap-save filter
 }gaussian, *GAUSSIAN;
 
